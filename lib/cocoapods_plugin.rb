bc42cd13f082f3f63b4426524cef3415f519c4f1
青木 <qingmu@2dfire.com>
2018-12-25 11:16:24 +0800

    fix crash

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -7,7 +7,8 @@ Pod::HooksManager.register('cocoapods-input-output-cleaner', :post_install) do |
 	Pod::UI.section('Tdfire: auto clean input and output files') do
 		context.umbrella_targets.map(&:user_targets).flatten.uniq.each do |t|
       phase = t.shell_script_build_phases.find { |p| p.name.include?(Pod::Installer::UserProjectIntegrator::TargetIntegrator::COPY_PODS_RESOURCES_PHASE_NAME) }
-
+      next unless phase 
+      
       max_input_output_paths = 0
       input_output_paths = phase.input_paths.count + phase.output_paths.count
       Pod::UI.message "Tdfire: input paths and output paths count for #{t.name} : #{input_output_paths}"
@@ -16,7 +17,6 @@ Pod::HooksManager.register('cocoapods-input-output-cleaner', :post_install) do |
       	phase.input_paths.clear
       	phase.output_paths.clear
       end
-
     end
 
     context.umbrella_targets.map(&:user_project).each do |project|
